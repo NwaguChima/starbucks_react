80c32c91d6de36928931a107c6c76f4521ed3afa
NwaguChima <nwaguchima48@gmail.com>
2022-07-30 23:28:49 +0100

    setup routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Fade } from "react-awesome-reveal";
 import "./App.css";
 
 function App() {
-  return <div className="app">Hello world</div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Header />
+            <HomeScreen />
+            <Fade>
+              <Footer />
+            </Fade>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
