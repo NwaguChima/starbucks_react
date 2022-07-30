e14f31d4a50fd6ed494bdc4456ef79746df5b059
NwaguChima <nwaguchima48@gmail.com>
2022-07-30 23:42:35 +0100

    setup header left

--- src/App.js
+++ src/App.js
@@ -3,19 +3,26 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { Fade } from "react-awesome-reveal";
 import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}>
-            <Header />
-            <HomeScreen />
-            <Fade>
-              <Footer />
-            </Fade>
-          </Route>
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <HomeScreen />
+                <Fade>
+                  <Footer />
+                </Fade>
+              </>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <Link className="header__logo" to="/">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png"
+            alt=""
+          />
+        </Link>
+        <Link className="header__link" to="/menu">
+          Menu
+        </Link>
+        <Link className="header__link" to="/menu">
+          Rewards
+        </Link>
+        <Link className="header__link" to="/menu">
+          Gift Cards
+        </Link>
+      </div>
+
+      <div className="header__right"></div>
+    </div>
+  );
+};
+
+export default Header;
