ebc51dc09fab43efde0d43f8365270e6580d2958
NwaguChima <nwaguchima48@gmail.com>
2022-07-31 01:16:11 +0100

    added styles to signin button

--- src/components/signInButton/SignInButton.jsx
+++ src/components/signInButton/SignInButton.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./SignInButton.css";
 
 const SignInButton = () => {
-  return <div>SignInButton</div>;
+  return (
+    <Link className="signInButton" to="/account/signin">
+      Sign In
+    </Link>
+  );
 };
 
 export default SignInButton;
