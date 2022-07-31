d372a99b1163a00efe93fa9e1bb3a4520e6cdb05
NwaguChima <nwaguchima48@gmail.com>
2022-07-31 01:28:53 +0100

    setup mini heading comp

---
+++ src/components/heading/Heading.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Heading.css";
+
+const Heading = ({ heading }) => {
+  return <div className="heading">Heading</div>;
+};
+
+export default Heading;
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Fade } from "react-awesome-reveal";
 import { Link } from "react-router-dom";
+import Heading from "../components/heading/Heading";
 
 const HomeScreen = () => {
   return (
@@ -22,7 +23,16 @@ const HomeScreen = () => {
           </p>
           <button>Learn more</button>
         </div>
-        <div className="homeScreen__bottomRight"></div>
+        <div className="homeScreen__bottomRight">
+          <img
+            src="https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-68026.jpg"
+            alt="homescreen cover"
+          />
+        </div>
+      </Fade>
+
+      <Fade>
+        <Heading heading="TODAY IS YOURS" />
       </Fade>
     </div>
   );
