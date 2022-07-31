fba81ee98dd880eb1774d9ef750cba09d68c23ed
NwaguChima <nwaguchima48@gmail.com>
2022-07-31 01:12:59 +0100

    rendered menu items

--- src/components/framerMotion/MenuItem.jsx
+++ src/components/framerMotion/MenuItem.jsx
@@ -3,23 +3,6 @@ import { Link } from "react-router-dom";
 import "./MenuItem.css";
 import { Fade } from "react-awesome-reveal";
 
-const variants = {
-  open: {
-    y: 0,
-    opacity: 1,
-    transition: {
-      y: { stiffness: 1000, velocity: -100 },
-    },
-  },
-  closed: {
-    y: 50,
-    opacity: 0,
-    transition: {
-      y: { stiffness: 1000 },
-    },
-  },
-};
-
 function MenuItem({ type, image }) {
   return (
     <Fade>
--- src/components/framerMotion/MenuLink.jsx
+++ src/components/framerMotion/MenuLink.jsx
@@ -1,17 +1,43 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
+import ChevronRightRoundedIcon from "@material-ui/icons/ChevronRightRounded";
+import ChevronLeftRoundedIcon from "@material-ui/icons/ChevronLeftRounded";
 import "./MenuLink.css";
 
+const variants = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 },
+    },
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 },
+    },
+  },
+};
+
 const MenuLink = ({ link, path, icon, goBackIcon, onClick, width }) => {
+  console.log("path", path);
   return (
     <>
-      <Link to={path}>
+      <Link to={""}>
         <motion.li
           style={{ width }}
           onClick={onClick}
           variants={variants}
-        ></motion.li>
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+        >
+          {goBackIcon && <ChevronLeftRoundedIcon fontSize="large" />}
+          {link}
+          {icon && <ChevronRightRoundedIcon fontSize="large" />}
+        </motion.li>
       </Link>
     </>
   );
