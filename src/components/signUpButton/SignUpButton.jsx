de4ffd2e9a1f15b7d21a8bbd9ef2bdea664e2f09
NwaguChima <nwaguchima48@gmail.com>
2022-07-31 01:17:53 +0100

    signup done

--- src/components/signUpButton/SignUpButton.jsx
+++ src/components/signUpButton/SignUpButton.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./SignUpButton.css";
 
 const SignUpButton = () => {
-  return <div>SignUpButton</div>;
+  return (
+    <Link className="signUpButton" to="account/create">
+      Join Now
+    </Link>
+  );
 };
 
 export default SignUpButton;
