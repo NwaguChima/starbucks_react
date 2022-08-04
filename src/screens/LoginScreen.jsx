1359a9435886b94f6a5df335ac6b9087272ae8b7
NwaguChima <nwaguchima48@gmail.com>
2022-08-04 01:36:33 +0100

    setup signupscreen header

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -41,7 +41,7 @@ const LoginScreen = () => {
         <Link to="/">
           <img
             src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png"
-            alt=""
+            alt="logo starbucks"
           />
         </Link>
         <div className="loginScreen__info">
--- src/screens/SignupScreen.jsx
+++ src/screens/SignupScreen.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./SignupScreen.css";
 
 const SignupScreen = () => {
-  return <div>SignupScreen</div>;
+  return (
+    <div className="signupScreen">
+      <div className="signupScreen__header">
+        <Link to="/">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png"
+            alt="logo starbucks"
+          />
+        </Link>
+      </div>
+      <h1 className="signupScreen__heading">Create an account</h1>
+    </div>
+  );
 };
 
 export default SignupScreen;
