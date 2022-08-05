4ea67879998c40ca551b7e564f9d9e26f20f1355
NwaguChima <nwaguchima48@gmail.com>
2022-08-05 02:25:58 +0100

    setup menu item

--- src/components/menuItem/MenuItem.jsx
+++ src/components/menuItem/MenuItem.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { Fade } from "react-awesome-reveal";
+import { Link } from "react-router-dom";
 import "./MenuItem.css";
 
-const MenuItem = () => {
-  return <div>MenuItem</div>;
+const MenuItem = ({ type, image, path }) => {
+  return (
+    <Fade>
+      <Link to={path} className="menuItem">
+        <img src={image} alt={type} className="menuItem__image" />
+        <h4 className="menuItem__type">{type}</h4>
+      </Link>
+    </Fade>
+  );
 };
 
 export default MenuItem;
