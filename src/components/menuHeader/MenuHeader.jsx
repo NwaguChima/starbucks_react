dc06fc4719b85a9c9f41990a32b5c051e72d7fb2
NwaguChima <nwaguchima48@gmail.com>
2022-08-05 02:03:29 +0100

    setup menu header

--- src/components/menuHeader/MenuHeader.jsx
+++ src/components/menuHeader/MenuHeader.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./MenuHeader.css";
 
 const MenuHeader = () => {
-  return <div>MenuHeader</div>;
+  const [index, setIndex] = useState(0);
+
+  return (
+    <div className="menuHeader">
+      <div className="menuHeader__links">
+        <Link
+          to="/menu/allproducts"
+          className={`${index === 0 && "menuHeader__link--active"}`}
+          onClick={() => setIndex(0)}
+        >
+          All Products
+        </Link>
+        <Link
+          to="/menu/featured"
+          className={`${index === 1 && "menuHeader__link--active"}`}
+          onClick={() => setIndex(1)}
+        >
+          Featured
+        </Link>
+        <Link to="/menu/previous-orders">Previous Orders</Link>
+        <Link to="/menu/favourite">Favourite Product</Link>
+      </div>
+    </div>
+  );
 };
 
 export default MenuHeader;
